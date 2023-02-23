86bc81034246c66ef0f2fb2cdd2369502630c623
devinmuzzy <68883649+devinmuzzy@users.noreply.github.com>
2023-02-23 08:25:44 -0800

    added boilerplate

--- server.py
+++ server.py
@@ -5,31 +5,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>BlueServer</h1>"
+    return "<h1>Classification Server</h1>"
 
-#/api/blue?name=name
-@app.route("/api/blue", methods=['GET'])
-def blue():
-    if request.method != 'GET':
-        return "<script>alert('invalid method')</script>"
-    elif 'name' in request.args:
-        return f"<h1 style='color:blue'>BlueServer says hello {request.args['name']}!</h1>"
-    else:
-        return "<script>alert('invalid query string')</script>"
-
-#/api/blue?name=name
-@app.route("/api/jsonBlue", methods=['POST'])
-def jsonBlue():
+@app.route("/classification-service/classify", methods=['POST'])
+def classify_route():
+    # parse for the processed image
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         json = request.json
-        json['visitedBlue'] = 'true'
-        return json
+        # example of how parsing data should look
+        json['example'] = 'new data'
+        image = json['image']
+        uid = json['uid']
+        data = classify(image,uid)
+        return data
     else:
         return 'Content-Type not supported!'
 
+def classify(array, uid):
+    # future classification code
 
+    # temporarily update progress bar
+    update_bar(uid=uid, progress=50)
 
+    # return array of 1's and 0's
+    return array
+
+def update_bar(uid, progress):
+    payload = {'uid':uid, 'progress':progress}
+    r = requests.get('http://main-server/progress',params=payload)
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
