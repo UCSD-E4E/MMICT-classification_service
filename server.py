d8b3358ed93e19df45012be3c141280b885c25c9
Jasonnyang <36582468+Jasonnyang@users.noreply.github.com>
2023-07-31 10:18:54 -0700

    Update server.py

--- server.py
+++ server.py
@@ -116,5 +116,5 @@ def classify(array, classifier_id):
   
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+    app.run(debug=False, port=5001, host='0.0.0.0')
     # app.run(debug=True, port=5001)
